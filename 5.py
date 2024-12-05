1c9397a3e76a2895a212535385f373a308a200d2
Laas H. Lumberg <laas.lumberg@gmail.com>
2024-12-05 17:17:30 +0200

    5

--- 5.py
+++ 5.py
@@ -1,17 +1,59 @@
 import time
 
+def parser(lines):
+    for i in range(len(lines)): lines[i] = lines[i].strip()
+
+    sundmused = {}
+    i = 1
+    while lines[i] != "Naabrid:":
+        tegevus = lines[i].split(": ")
+        sundmused[tegevus[0]] = int(tegevus[1])
+        i += 1
+
+    naabrid = lines[i+1:]
+    for i in range(len(naabrid)):
+        naabrid[i] = naabrid[i].split(": ")[1].split(", N")
+        for j in range(len(naabrid[i])):
+            if naabrid[i][j][0] != "N": 
+                naabrid[i][j] = "N" + naabrid[i][j]
+    return [sundmused, naabrid]
+
+def skoor(sundmused, naaber):
+    skoor = 0
+    for tegevus in naaber:
+        skoor += sundmused[tegevus]
+    return skoor
+
 def main(filename):
-    lines = open(filename).readlines()
+    lines = open(filename, encoding="utf-8").readlines()
     
+    parsed = parser(lines)
+    sundmused = parsed[0]
+    naabrid = parsed[1]
 
+    for sundmus in sundmused:
+        print(sundmus)
+    
+    print()
+    for naaber in naabrid:
+        print(naaber)
+    print()
 
-    return 1
+    maxSkoor = 10
+    kahtlane = []
+
+    for i in range(len(naabrid)):
+        print(str(i) + ": " + str(skoor(sundmused, naabrid[i])))
+        if skoor(sundmused, naabrid[i]) > maxSkoor:
+            kahtlane.append(i)
+
+    return len(kahtlane)
     
 
 if __name__ == "__main__":
     start = time.time()
 
-    print(main("test.txt"))
+    print(main("input.txt"))
 
     end = time.time()
     print("Aeg: " + str(end-start))
